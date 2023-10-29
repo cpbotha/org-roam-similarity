8015065db870fbe19eb35eea68b4e595864f2d3c
Charl P. Botha <cpbotha@vxlabs.com>
2023-10-29 14:40:32 +0200

    Add frontmatter + check for already markdown

--- export-org-roam-nodes-to-md.el
+++ export-org-roam-nodes-to-md.el
@@ -1,40 +1,50 @@
 ;; without this, I was getting void-function org-all-archive-files
 (require 'org-archive)
 (require 'org-roam)
+
 ;; emacs-lisp code to iterate over org-roam-node-list
 ;; for each node, open the file, goto-char to "point"
 ;; then use (org-export-as 'md (org-at-heading-p) nil t)
 ;; to export as markdown, and save that to a file named after the node's "id"
-;; write the code below:
-
 (defun export-node-as-markdown (node)
   "Export the given node as markdown and save it in a file."
   (let* ((file (org-roam-node-file node))
          (id (org-roam-node-id node))
          (point (org-roam-node-point node))
          (title (org-roam-node-title node))
-         ;; silence: Need absolute ‘org-attach-id-dir’ to attach in buffers without filename
+         (file-mtime (org-roam-node-file-mtime node))
+         ;; silence "Need absolute ‘org-attach-id-dir’ to attach in buffers without filename" error
          (org-attach-directory "/tmp"))
     
     ;;(message "%s - %s" file title)
     (with-temp-buffer
       (insert-file-contents file)
       (goto-char point)
-      ;; TODO: if source buffer is already md, don't export!
-      (let* ((md (org-export-as 'md (org-at-heading-p) nil t )))
+      ;; if source buffer is already md, just copy over the whole thing
+      ;; if not, export the current node, which could be a heading within an org file
+      (let* ((md (if (string= "md" (file-name-extension file))
+                     (buffer-string)
+                   (org-export-as 'md (org-at-heading-p) nil t ))))
         (with-temp-file (concat id ".md")
-          (insert (format "---\ntitle: %s\n---\n" title))
+          ;; add yaml frontmatter that can be parsed out by python bits
+          ;; escape "-characters in title
+          ;; store mtime as iso8601 timestamp
+          (insert (format "---\ntitle: \"%s\"\nmtime: %s\n---\n"
+                          (replace-regexp-in-string "\"" "\\\\\"" title)
+                          (format-time-string "%FT%T%z" file-mtime)))
           (insert md))))))
 
-;; the exporter breaks on includes
+;; the exporter breaks on includes, so here we temporarily blot out that function
 (cl-letf (((symbol-function 'org-export-expand-include-keyword) #'(lambda())))
-  (let ((default-directory "/tmp")
+  (let ((default-directory "/tmp/bleh/")
         (node-list (org-roam-node-list)))
     (message "======> exporting %d nodes" (length node-list))
     (dolist (node node-list)
       (export-node-as-markdown node))
     )
-  (message "DONE.")
+  (message "DONE."))
 
-  )
 
+;; scratchpad
+;;(org-roam-id-find "4063BD96-06F1-4529-96D1-32C0CBC6F4BA")
+;;(org-roam-node-find)
