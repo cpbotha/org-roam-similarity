35dc42653797492073e8cfe3fd973bbb0c73ee55
Charl P. Botha <cpbotha@vxlabs.com>
2023-12-03 18:14:23 +0200

    Add inference part to lisp

--- org-roam-similarity.el
+++ org-roam-similarity.el
@@ -59,3 +59,73 @@
     (message "DONE.")))
 
 
+;; add function to org-roam-mode-sections (by default it has two: backlinks and reflinks)
+(require 'url)
+(require 'json)
+
+(defun ors--get-similar-nodes (text)
+  (let* ((url "http://localhost:3814/similar/")
+         (url-request-method "POST")
+         (url-request-extra-headers '(("Content-Type" . "application/json")))
+         ;; have to utf-8 encode json-encode's result
+         ;; see https://lists.gnu.org/archive/html/emacs-devel/2020-06/msg00515.html
+         (url-request-data (encode-coding-string (json-encode `(("text" . ,text))) 'utf-8))
+         (response-buffer (url-retrieve-synchronously url))
+         json-array)
+    (with-current-buffer response-buffer
+      (goto-char url-http-end-of-headers)
+      ;; this is a vector of vectors
+      (setq json-array (json-read))
+      (kill-buffer))
+    ;; json-array is a vector with nested two-element (id, similarity-value) vectors
+    ;; we mapcar over it, converting the outer vec to list, while we extract
+    ;; the first element (id) of each 2-element vector
+    (mapcar (lambda (v) (aref v 0)) json-array)))
+
+
+;;;###autoload
+(defun org-roam-similarity-section (node)
+  "The similarity section for NODE."
+
+  ;; NOTES:
+  ;; - org-roam-node-marker shows how to find buffer visiting node file
+  
+  ;; get string text of current node
+  ;; send that to the backend to get list of nodes
+  (let* ((file (org-roam-node-file node))
+         (id (org-roam-node-id node))
+         (point (org-roam-node-point node))
+         (buffer (or (find-buffer-visiting file)
+                     (find-file-noselect file)))
+         node-text
+         node-ids)
+    (with-current-buffer buffer
+      (save-excursion
+        (goto-char point)
+        (setq node-text (if (or (string= "md" (file-name-extension file)) (= point 1))
+                            (buffer-string)
+                          (progn
+                            (let* ((element (org-element-at-point))
+                                   (begin (org-element-property :begin element))
+                                   (end (org-element-property :end element)))
+                              (buffer-substring begin end)) )
+                          ))))
+
+    ;; TODO: if region selected, then only search for that region
+    ;; obviously the most similar node will be us, so we see ourselves out haha
+    (setq node-ids (remove id (ors--get-similar-nodes node-text)))
+
+    (when node-ids
+      (magit-insert-section (org-roam-similarity)
+        (magit-insert-heading "Similar notes:")
+        (dolist (node-id node-ids)
+          ;;
+          (when-let ((snode (org-roam-node-from-id node-id)))
+            (org-roam-node-insert-section
+             :source-node snode
+             :point (org-roam-node-point snode)
+             :properties nil))
+          (insert ?\n))))))
+
+(provide 'org-roam-similarity)
+
