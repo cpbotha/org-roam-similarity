80d6840347256c4a4e114469a1813d5c2cad43b5
Charl P. Botha <cpbotha@vxlabs.com>
2024-01-12 22:44:38 +0200

    Prefer CPU instead of MPS on mac

--- org_roam_similarity/utils.py
+++ org_roam_similarity/utils.py
@@ -1,3 +1,5 @@
+import logging
+
 import torch
 import torch.nn.functional as F
 from transformers import AutoModel, AutoTokenizer
@@ -6,7 +8,7 @@ MODEL_LUT = ["sentence-transformers/multi-qa-MiniLM-L6-cos-v1", "jinaai/jina-emb
 
 
 # See https://www.sbert.net/docs/pretrained_models.html for more. Currently we support the following:
-# - nreimers/MiniLM-L6-H384-uncased == multi-qa-MiniLM-L6-cos-v1
+# - multi-qa-MiniLM-L6-cos-v1
 #   max context length 512
 # - jinaai/jina-embeddings-v2-small-en
 #   max context length 8192 but much slower; needs NVIDIA GPU with a few gigs
@@ -21,7 +23,11 @@ def get_model_and_tokenizer(model_name="jinaai/jina-embeddings-v2-small-en"):
         )
         print(f"Found device: {device}")
         model = AutoModel.from_pretrained(model_name, trust_remote_code=True)
-        model.to(device)
+        if device.type == "mps":
+            # on my M1Pro 10C with multi-qa-MiniLM-L6-cos-v1, MPS is often 2x slower and takes much more RAM: 1.3GB vs 350MB
+            logging.warning("MPS often slower than CPU, sticking to CPU")
+        else:
+            model.to(device)
 
     tokenizer = AutoTokenizer.from_pretrained(model_name)
 
@@ -39,6 +45,7 @@ def mean_pooling(model_output, attention_mask):
 def encode(texts, tokenizer, model):
     # Tokenize sentences
     encoded_input = tokenizer(texts, padding=True, truncation=True, return_tensors="pt").to(model.device)
+    print(encoded_input["input_ids"].shape)
 
     # Compute token embeddings
     with torch.no_grad():
